91baa2b70b6727dbbf8cf152d8f0ccbf7a235d3c
Osusara Kammalawatta <osusara@calcey.com>
2024-11-22 00:46:36 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        display: ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        dark: "#121212",
+        light: "#fafafa",
+        accent: "#404040",
+      },
+    },
+  },
+  plugins: [],
+}; 
